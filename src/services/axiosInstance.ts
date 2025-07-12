1f25480b9ea8cff9fb5c72f3c23d6a2e3896fd81
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-12 17:37:33 +0500

    Add Axios instance

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,22 @@
+﻿import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://nuieee.onrender.com/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+// Add interceptor to include token
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
