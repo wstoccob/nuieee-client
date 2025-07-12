7c6d6d28cce6188a06f9d49970d6fd4c382b38c9
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-12 17:37:40 +0500

    Add App router

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,16 @@
+﻿import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/auth/LoginPage.tsx'
+import SuperAdminPage from '../pages/superadmin/SuperAdminPage.tsx'
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/auth/login" element={<LoginPage />} />
+                <Route path="/superadmin" element={<SuperAdminPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
