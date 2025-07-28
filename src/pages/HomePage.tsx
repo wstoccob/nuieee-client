3a880c0a4346c6dec8167f59fd21d107a9e152e1
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-28 14:14:36 +0500

    Update HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+﻿import { MainLayout } from "../components/Layouts/MainLayout/MainLayout.tsx";
+import {HeroSection}  from "../components/HomePage/HeroSection.tsx";
+import { BigTextSection } from "../components/HomePage/BigTextSection.tsx";
+
+export default function HomePage() {
+    return (
+        <MainLayout>
+            <main>
+                <HeroSection />
+                <BigTextSection />
+            </main>
+        </MainLayout>
+    );
+}
