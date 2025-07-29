04d91e2a75cc08ec738e4b9a97c66e9416acb573
Alinur Seisekov <alinur.seysekov@gmail.com>
2025-07-29 13:01:26 +0500

    Update HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,8 @@
 import {HeroSection}  from "../components/HomePage/HeroSection.tsx";
 import {BigTextSection} from "../components/HomePage/BigTextSection.tsx";
 import {AnnouncementTicker} from "../components/HomePage/AnnouncementTicker.tsx";
+import {AboutUsSection} from "../components/HomePage/AboutUsSection.tsx";
+import {EventsSection} from "../components/HomePage/EventsSection.tsx";
 
 export default function HomePage() {
     return (
@@ -10,6 +12,8 @@ export default function HomePage() {
                 <HeroSection />
                 <BigTextSection />
                 <AnnouncementTicker />
+                <AboutUsSection />
+                <EventsSection />
             </main>
         </MainLayout>
     );
